e250ca2527dc3a1b5b71d8bf50d82cca21433509
mynameisameed <msun_007_1@yahoo.com>
2025-08-31 23:02:00 -0400

    Refactor: Comment out Route53 record resource and output in main.tf for clarity

--- infra/hosted-zone/main.tf
+++ infra/hosted-zone/main.tf
@@ -8,18 +8,18 @@ variable "aws_lb_zone_id" {}
 #   private_zone = false
 # }
 
-resource "aws_route53_record" "lb_record" {
-  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
-  name    = var.domain_name
-  type    = "A"
+# resource "aws_route53_record" "lb_record" {
+#   zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+#   name    = var.domain_name
+#   type    = "A"
+#
+#   alias {
+#     name                   = var.aws_lb_dns_name
+#     zone_id                = var.aws_lb_zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
-  alias {
-    name                   = var.aws_lb_dns_name
-    zone_id                = var.aws_lb_zone_id
-    evaluate_target_health = true
-  }
-}
-
-output "hosted_zone_id" {
-  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
-}
+# output "hosted_zone_id" {
+#   value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+# }
