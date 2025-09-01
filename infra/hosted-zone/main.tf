957866c496a199a510015cf7220a9a57f2a0f704
mynameisameed <msun_007_1@yahoo.com>
2025-08-31 23:00:21 -0400

    Comment out unused Route53 resources in main.tf due to lack of available domain

--- infra/hosted-zone/main.tf
+++ infra/hosted-zone/main.tf
@@ -2,10 +2,11 @@ variable "domain_name" {}
 variable "aws_lb_dns_name" {}
 variable "aws_lb_zone_id" {}
 
-data "aws_route53_zone" "dev_proj_1_jhooq_org" {
-  name         = var.domain_name
-  private_zone = false
-}
+# Commented out Route53 resources as no domain is available for this lab
+# data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+#   name         = var.domain_name
+#   private_zone = false
+# }
 
 resource "aws_route53_record" "lb_record" {
   zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
